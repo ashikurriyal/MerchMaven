2ddd21532882f3e2e7d8e83101346cff5a4c1a3e
ashikurriyal <riyal.ashikur@gmail.com>
2024-08-16 19:00:47 +0600

    Updated the navbar footer and route setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-362px)]">
+        <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Routes/Routes";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
