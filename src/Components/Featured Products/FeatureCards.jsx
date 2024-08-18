fe8a7e66d167df0e7b490e87ae9ce38318f5bbad
ashikurriyal <riyal.ashikur@gmail.com>
2024-08-18 13:52:04 +0600

    Updated the search and sort by price function updated

--- src/Components/Featured Products/FeatureCards.jsx
+++ src/Components/Featured Products/FeatureCards.jsx
@@ -1,43 +1,98 @@
+
 import { Link } from "react-router-dom";
 
 const FeatureCards = ({ item }) => {
-  const {productName, productImage, description, price, category, ratings, creationDate , brand} = item
-  console.log(item)
+
+  const {
+    productName,
+    productImage,
+    description,
+    price,
+    category,
+    ratings,
+    creationDate,
+    brand,
+  } = item;
+  //   console.log(item)
+
+ 
+
   return (
     <div>
-        <div className=" max-w-sm md:h-[600px] bg-white border-2 border-black rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl">
-            <div className="flex justify-center">
-                <img className="rounded-t-lg h-60" src={productImage} alt="" />
-            </div>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{productName}</h5>
-                </a>
-                <p className="text-gray-600 text-base mb-2 font-medium">Location: {description}</p>
-                <p className="text-gray-600 text-base mb-2 font-medium">Category: <span className="text-base font-medium text-green-500">{category}</span></p>
-                <p className="text-gray-600 text-base mb-2 font-medium">Brand: <span className="text-base font-medium text-green-500">{brand}</span></p>
-                <p className="text-gray-600 text-base mb-2 font-medium">Updated Date: <span className="text-base font-medium text-green-500">{creationDate}</span></p>
-                <p className="text-gray-600 text-base mb-2 font-medium">Ratings: <span className="text-base font-medium text-green-500">{ratings}</span></p>
-                <div className="flex justify-between items-center mt-6">
-                    <div className="">
-                        <p className=" text-xl font-bold text-yellow-400 mb-2">${price}</p>
-                    </div>
-                    <div>
-                        <Link to={``} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black btn btn-warning btn-md">
-                            <span className="inline-flex items-center"> {/* Container for text and SVG */}
-                                <span className="mr-1">Details</span> {/* Margin to separate text and SVG */}
-                                <svg className="rtl:rotate-180 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </span>
-                        </Link>
-                    </div>
-                </div>
-
-            </div>
+      
+      <div className=" max-w-sm md:h-[600px] bg-white border-2 border-black rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl">
+        <div className="flex justify-center">
+          <img className="rounded-t-lg h-60" src={productImage} alt="" />
         </div>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {productName}
+            </h5>
+          </a>
+          <p className="text-gray-600 text-base mb-2 font-medium">
+            Location: {description}
+          </p>
+          <p className="text-gray-600 text-base mb-2 font-medium">
+            Category:{" "}
+            <span className="text-base font-medium text-green-500">
+              {category}
+            </span>
+          </p>
+          <p className="text-gray-600 text-base mb-2 font-medium">
+            Brand:{" "}
+            <span className="text-base font-medium text-green-500">
+              {brand}
+            </span>
+          </p>
+          <p className="text-gray-600 text-base mb-2 font-medium">
+            Updated Date:{" "}
+            <span className="text-base font-medium text-green-500">
+              {creationDate}
+            </span>
+          </p>
+          <p className="text-gray-600 text-base mb-2 font-medium">
+            Ratings:{" "}
+            <span className="text-base font-medium text-green-500">
+              {ratings}
+            </span>
+          </p>
+          <div className="flex justify-between items-center mt-6">
+            <div className="">
+              <p className=" text-xl font-bold text-teal-600 mb-2">${price}</p>
+            </div>
+            <div>
+              <Link
+                to={``}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black btn btn-warning btn-md"
+              >
+                <span className="inline-flex items-center">
+                  {" "}
+                  {/* Container for text and SVG */}
+                  <span className="mr-1">Details</span>{" "}
+                  {/* Margin to separate text and SVG */}
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
--- src/Components/Featured Products/FeaturedProducts.jsx
+++ src/Components/Featured Products/FeaturedProducts.jsx
@@ -5,6 +5,8 @@ import FeatureCards from "./FeatureCards";
 const FeaturedProducts = () => {
 
     const [product, setProduct] = useState([]);
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
             fetch('data.JSON')
@@ -13,16 +15,54 @@ const FeaturedProducts = () => {
             
     },[])
     console.log(product)
+
+    const handleSort = (order) => {
+        setSortOrder(order);
+      };
+      const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+      };
+
+      const sortProducts = product.slice().sort((a,b) => {
+        if(sortOrder === 'asc'){
+            return a.price - b.price;
+        } else {
+            return b.price - a.price;
+        }
+      })
+
+      const filterProducts = sortProducts.filter(product => product.productName.toLowerCase().includes(searchTerm.toLowerCase()))
     return (
         <div className="max-w-7xl mx-auto lg:space-y-10 space-y-5">
             <div className="text-center space-y-4">
                 <h1 className="lg:text-5xl text-3xl font-bold">Featured Product</h1>
                 <p className="lg:text-xl text-lg font-normal">Discover our featured product and find your dream gadgets today. Browse through the latest listings and take advantage of the best deals on the market.</p>
             </div>
+            <div className="flex justify-between mb-4">
+        <div>
+          <input
+            type="text"
+            placeholder="Search by product name"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="border-2 border-teal-600 text-teal-600 text-base lg:font-medium font-normal rounded-md p-3"
+          />
+        </div>
+        <div>
+          <select
+            onChange={(e) => handleSort(e.target.value)}
+            value={sortOrder}
+            className="border-2 border-teal-600 text-teal-600 text-base lg:font-medium font-normal  rounded-md p-3"
+          >
+            <option value="asc">Price: Low to High</option>
+            <option value="desc">Price: High to Low</option>
+          </select>
+        </div>
+      </div>
 
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center gap-4">
                 {
-                    product.map(item => <FeatureCards key={item.id} item = {item}></FeatureCards>) //slice(0,4)
+                    filterProducts.map(item => <FeatureCards key={item.id} item = {item}></FeatureCards>) //slice(0,4)
                 }
             </div>
 
